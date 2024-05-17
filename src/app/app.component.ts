7fe29f246723a92c19b9b3ef0497112d2a86da3a
MCVdev <mateocarvajalvergara333@gmail.com>
2024-05-17 03:01:41 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private apiUrl = 'http://127.0.0.1:8080/festivos';
+
+  years: number[] = [];
+  holidays: any[] = [];
+  displayedColumns: string[] = ['nombre', 'dia', 'mes'];
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+      // Obtener el año actual
+      const currentYear = new Date().getFullYear();
+      // Generar las opciones de años
+      for (let i = currentYear; i > currentYear - 50; i--) {
+        this.years.push(i);
+      }
+  }
+
   title = 'holiday_validator_front';
+
+  validateDate(dateValue: string): void {
+    if(dateValue) {
+      const date = new Date(dateValue);
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1; // getMonth() returns 0-11, so add 1 to get 1-12
+      const day = date.getDate() + 1;
+
+      this.http.get(`${this.apiUrl}/verificar/${year}/${month}/${day}`, { responseType: 'text' }).subscribe(res=> {
+        if(res == 'no es festivo!!') {
+          this.snackBar.open('No es festivo!!', '', {
+            duration: 3000,
+            panelClass: 'custom-snackbar'
+          });
+        } else {
+          this.snackBar.open('Si es festivo!!', '', {
+            duration: 3000,
+            panelClass: 'custom-snackbar'
+          });
+        }
+      },
+      error => {
+        console.error('Error:', error);
+      });  
+    } else {
+      this.snackBar.open('Selecciona una fecha','', {
+        duration: 3000,
+        panelClass: 'custom-snackbar'
+      });
+    }
+    }
+
+  validateYear(yearSelect: HTMLSelectElement) {
+    const year = +yearSelect.value;
+    this.http.get<any>(`${this.apiUrl}/${year}`).subscribe((res) => {
+      this.holidays = res;
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';  
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatTableModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
